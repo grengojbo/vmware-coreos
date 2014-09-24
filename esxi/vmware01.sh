13a5988eb9ef4d38ce14b151d77b5f85b4dabbf9
Oleg Dolya <oleg.dolya@gmail.com>
2014-09-24 19:29:46 +0300

    fix bug

---
+++ esxi/vmware01.sh
@@ -0,0 +1,9 @@
+VM_SSH="root@<ESXI IP>"
+VM_HOST_NAME="noname"
+
+# Path of Datastore to store CoreOS
+DATASTORE_PATH=/vmfs/volumes/datastore1
+
+# VM Network to connect CoreOS to
+VM_NETWORK="VM Network"
+VM_INT="vmxnet3"
